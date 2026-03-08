24f28f3ffc7852d4197995d71c9a67f434a4428d
fj.gutierrezs91 <fj.gutierrezs91@gmail.com>
2026-03-08 04:04:16 +0100

    Added update in all rows in the table.

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -5,7 +5,9 @@ echo "Starting 10ms stream... Press Ctrl+C to stop."
 while true; do
   # Use -c to run the command and -q for quiet mode
   psql -d javi -c "UPDATE articles SET stock_count = stock_count + 1 WHERE name = 'cafe';" > /dev/null 2>&1
-  
+  psql -d javi -c "UPDATE articles SET stock_count = stock_count + 1 WHERE name = 'balloons';" > /dev/null 2>&1
+  psql -d javi -c "UPDATE articles SET stock_count = stock_count + 1 WHERE name = 'espresso';" > /dev/null 2>&1
+
   # sleep 0.01 is 10ms
   sleep 0.01
 done
