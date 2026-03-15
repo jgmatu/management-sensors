b297a3a6f8e0673c79236ed1ec2b7dd462517500
fj.gutierrezs91 <fj.gutierrezs91@gmail.com>
2026-03-15 03:57:55 +0100

    Remove openssl deps

--- server/deps/deps.sh
+++ server/deps/deps.sh
@@ -2,5 +2,5 @@
 
 # Download deps
 #
-# boost_1_90_0.tar.gz  Botan-3.10.0.tar.xz  liboqs-0.15.0-rc2.zip  libpqxx-8.0.0-rc5.zip 
-# openssl-3.5.5.tar.gz  oqs-provider-0.11.0.zip  postgresql-18.3.tar.gz
+# boost_1_90_0.tar.gz  Botan-3.10.0.tar.xz libpqxx-8.0.0-rc5.zip 
+# postgresql-18.3.tar.gz
