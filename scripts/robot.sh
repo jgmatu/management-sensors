31f4a3749fd69e659648ea7b33d1177d4370c5d3
fj.gutierrezs91 <fj.gutierrezs91@gmail.com>
2026-03-18 15:39:49 +0100

    Move more accesible robot test procedure to development

---
+++ scripts/robot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# scripts/robot.sh
+
+# Ir a la raíz del repo desde scripts/
+cd "$(dirname "$0")/.." || exit 1
+
+# Crear carpeta de salida para Robot
+mkdir -p tests/robot/output
+
+# Ejecutar todas las suites Robot
+robot -d tests/robot/output tests/robot/tests
